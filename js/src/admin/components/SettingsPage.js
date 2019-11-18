ca9d7e8095a52345a3a7b106affffb7d2a2d7a3c
cxsquared <cxsquared@gmail.com>
2019-11-17 21:26:07 -0500

    Properly using the store to get all users

--- js/src/admin/components/SettingsPage.js
+++ js/src/admin/components/SettingsPage.js
@@ -2,43 +2,58 @@ import app from 'flarum/app';
 import Page from 'flarum/components/Page';
 import User from 'flarum/models/User';
 import username from 'flarum/helpers/username';
+import LoadingIndicator from 'flarum/components/LoadingIndicator';
 
 export default class SettingsPage extends Page {
     init() {
         super.init();
+
+        this.users = null;
+
+        this.renderUsers = this.renderUsers.bind(this);
+
+        app.store.find('users').then(users => {
+            this.users = users;
+
+            m.redraw();
+        });
     }
 
     view() {
-        const users = app.forum.attribute("cxsquared-user-management.data")['users'].map(u => new User(u, app.store));
-
         return (
             <div className="UserPage container">
-                <div className="Headers">
-                    <legend>
-                        Username 
-                    </legend>
-                    <legend>
-                        Discussions
-                    </legend>
-                    <legend>
-                        Comments
-                    </legend>
-                </div>
-                {users.map((u, i) => (
-                    <div className={`UserRow ${i % 2 === 0 ? "Even" : "Odd"}`}>
-                        <div className="Number">{i + 1}</div>
-                        <div className="UserData">
-                            <a href={`${app.forum.attribute('baseUrl')}/u/${u.data['username']}`} target="_blank">{u.data['username']}</a>
-                        </div>
-                        <div className="UserData">
-                            {u.data['discussion_count']}
-                        </div>
-                        <div className="UserData">
-                            {u.data['comment_count']}
-                        </div>
-                    </div>
-                ))}
+                {this.users ? this.renderUsers() : LoadingIndicator.component({className: 'LoadingIndicator--block'})}
             </div>
         );
+    }
+
+    renderUsers() {
+        return ([
+            <div className="Headers">
+                <legend>
+                    Username
+                </legend>
+                <legend>
+                    Discussions
+                </legend>
+                <legend>
+                    Comments
+                </legend>
+            </div>,
+            this.users.map((u, i) => (
+                <div className={`UserRow ${i % 2 === 0 ? "Even" : "Odd"}`}>
+                    <div className="Number">{i + 1}</div>
+                    <div className="UserData">
+                        <a href={`${app.forum.attribute('baseUrl')}/u/${u.username()}`} target="_blank">{u.username()}</a>
+                    </div>
+                    <div className="UserData">
+                        {u.discussionCount()}
+                    </div>
+                    <div className="UserData">
+                        {u.commentCount()}
+                    </div>
+                </div>
+            ))
+        ]);
     }
 }
